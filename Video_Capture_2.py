24efad41bd20f9f46adb7539b041109921e7943f
Sherry Courington <75915202+scouring@users.noreply.github.com>
2021-02-14 12:54:11 -0500

    Video Capture
    This module utilizes OpenCV to define a capture object, frame by frame, and displays the resulting frame. The 'q' button is enabled for an escape key.
    It's a work in progress, as lab 3 hasn't been uploaded yet. Let me know any comments, needs for improvement.

---
+++ Video_Capture_2.py
@@ -0,0 +1,21 @@
+# import the opencv library.
+import cv2
+
+# define a video capture object.
+vid = cv2.VideoCapture(0)
+
+while(True):
+    # Capture the video frame by frame.
+    ret, frame = vid.read()
+
+    # Display the resulting frame.
+    cv2.imshow('frame', frame)
+
+    # the 'q' is the quit button or any other.
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop, release the capture object.
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
